815183c2496a89c65d5e2826921c7ec756b6172c
andredutradev <andrelsv.dutra@gmail.com>
2024-01-11 16:22:07 -0300

    att

---
+++ pages/viewArticle.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Pesquisa de Artigos | UPE Surubim</title>
+    <!-- Importação de estilos -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <img src="../assets/images/logoUpeSemBack.png" alt="header_logo" class="logo-min">
+        <nav>
+            <ul class="nav_menu">
+                <li><a href="" class="link_nav_menu">Quem Somos?</a></li>
+                <li><a href="" class="link_nav_menu">Fale Conosco</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="view_article_section">
+            <h3>Visualizando Artigo</h3>
+            <div class="view_article">
+                <div class="cover_article">
+                    <img src="../assets/images/testeimg.png" alt="" class="img_cover">
+                </div>
+                <div class="description_article_info">
+                    <h4><span class="detail_text">Título: </span>Exemplo de texto teste.</h4>
+                    <h4><span class="detail_text">Autores: </span>Exemplo de texto teste.</h4>
+                    <h4><span class="detail_text">Categoria: </span>Exemplo de texto teste.</h4>
+                    <h4><span class="detail_text">Data de postagem: </span>Exemplo de texto teste.</h4>
+                    <div class="text_description">
+                        <h4><span class="detail_text">Descrição: </span></h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore optio architecto quos reprehenderit id distinctio sit, delectus enim sunt totam quidem doloremque eos perferendis ipsum aliquam ducimus sapiente eaque soluta!</p>
+                    </div>
+                    <button class="view_item_btn"><a href="">Abrir Artigo</a></button>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Select2 JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
